3813d8bba9d651545cd7c46e7e1e70d2c66ecddb
guialvesf <guifraga.alves@gmail.com>
2023-04-24 11:00:03 -0300

    First commit

---
+++ DumpRepos.ps1
@@ -0,0 +1,21 @@
+function Get-Repos {
+
+    if (!$args) {
+        write-host ("Usage: ./DumpRepos <AzureDevops URL> <Basic auth>")
+        Exit
+    }
+
+    $URL = $args[0]
+    $Basic = $args[1]
+
+    $Projects = C:\Windows\system32\curl.exe "$URL/DefaultCollection/_apis/projects?\$top=3000&api-version=5.1" -H "Authorization: Basic $Basic" | convertfrom-json
+    
+    foreach ($Project in $Projects.value) {
+        $ProjectName = $Project.name
+        $Repos = C:\Windows\system32\curl.exe "$URL/DefaultCollection/$ProjectName/_apis/git/repositories?\$top=3000&api-version=5.1" -H "Authorization: Basic $Basic" | convertfrom-json
+    
+        foreach ($Repo in $Repos.value) {
+            git -c http.extraHeader="Authorization: Basic $Basic" clone $Repo
+        }
+    }
+}    
